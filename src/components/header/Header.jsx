b1afdfadb6aba4e4cd0ce54ae687f9bef49cbb71
ArnabBanik-repo <arnabbanik9@gmail.com>
2022-06-21 16:59:29 +0530

    Completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import CTA from './CTA'
-import Socials from './Socials'
-import Me from '../../assets/me1.png'
-import './header.css'
+import React from "react";
+import CTA from "./CTA";
+import Socials from "./Socials";
+import Me from "../../assets/me1.png";
+import "./header.css";
 const Header = () => {
 	return (
 		<header id="header">
 			<div className="container header__container">
 				<h5>Hello I'm</h5>
-				<h1>Mr.Onion</h1>
+				<h1>Arnab Banik</h1>
 				<h5 className="text-light">Fullstack Developer</h5>
 
 				<CTA />
@@ -23,7 +23,7 @@ const Header = () => {
 				</a>
 			</div>
 		</header>
-	)
-}
+	);
+};
 
-export default Header
+export default Header;
